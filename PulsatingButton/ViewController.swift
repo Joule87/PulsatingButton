052e0f22173de66d25ba029ff346131903715b09
Julio Collado <jcollado@altimetrik.com>
2020-03-15 16:02:20 -0300

    PulsatingButton: Dummy example

--- PulsatingButton/ViewController.swift
+++ PulsatingButton/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var youtubeImage: UIImageView!
+    @IBOutlet weak var nukeButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupNukeButton()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupYoutubeImage()
+    }
+    
+    func setupYoutubeImage() {
+        let rad = youtubeImage.frame.size.height
+        let pulse = Pulsing(radius: rad, position: youtubeImage.center)
+        
+        pulse.animationDuration = 0.8
+        pulse.backgroundColor = UIColor.red.cgColor
+        
+        self.view.layer.insertSublayer(pulse, below: youtubeImage.layer)
+    
+    }
+    
+    func setupNukeButton() {
+        nukeButton.isUserInteractionEnabled = true
+        let squareTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(addTapPulseAnimation))
+        nukeButton.addGestureRecognizer(squareTapGestureRecognizer)
+    }
+    
+    @objc func addTapPulseAnimation(){
+        let pulse = Pulsing(radius: 110, position: nukeButton.center, numberOfPulses: 1)
+        pulse.animationDuration = 0.8
+        pulse.backgroundColor = UIColor.blue.cgColor
+        
+        self.view.layer.insertSublayer(pulse, below: nukeButton.layer)
+        
+    }
+    
 }
 
